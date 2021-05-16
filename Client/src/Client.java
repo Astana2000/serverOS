c074e3f94d343f03205767d7459367c06b2e0871
Natalie Astafeva <popopo201497@gmail.com>
2021-05-16 18:38:21 +0300

    priority queue added

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,23 +1,29 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Random;
 
 public class Client {
+    static Random generator = new Random();
     public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.0.105", 8080);
+        try (Socket socket = new Socket("localhost", 7000);
              BufferedWriter writer = new BufferedWriter(
                      new OutputStreamWriter(socket.getOutputStream()));
              BufferedReader reader = new BufferedReader(
                      new InputStreamReader(socket.getInputStream()))
         ) {
             System.out.println("Connected to server");
-            String request = "Penza";
-            writer.write(request);
-            System.out.println("Request:" + request);
+            int processingTime = 5 + generator.nextInt(115);
+            int toAnswer = (generator.nextInt() % 2 + 2) %2;
+            int priority = generator.nextInt(5);
+            writer.write(processingTime);
+            writer.write(toAnswer);
+            writer.write(priority);
+
             writer.newLine();
             writer.flush();
 
             String response = reader.readLine();
-            System.out.println("Response:" + response);
+            System.out.println(response);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- SimpleServer/src/Server.java
+++ SimpleServer/src/Server.java
@@ -1,10 +1,22 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Server {
+    public static ArrayList<Integer> getMessage() {
+        return queue.poll();
+    }
+    static Queue<ArrayList<Integer>> queueFromHandler;
+    static Queue<ArrayList<Integer>> queue;
+    static Comparator<ArrayList<Integer>> comparator = Comparator.comparingInt(c -> c.get(2));
+
     public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(8080)) {
+        try (ServerSocket server = new ServerSocket(7000)) {
+            queue = new PriorityQueue(comparator);
             System.out.println("Server started!");
             while (true) {
                 try (
@@ -14,9 +26,21 @@ public class Server {
                         BufferedReader reader = new BufferedReader(
                                 new InputStreamReader(socket.getInputStream()))) {
                     System.out.println("Client connected");
-                    String request = reader.readLine();
-                    String response = "Hello from Server " + request.length();
+                    ArrayList<Integer> parameters = new ArrayList<>();
+                    parameters.add(reader.read()); //time
+                    parameters.add(reader.read()); //toAnswer
+                    parameters.add(reader.read()); //priority
+                    queue.add(parameters);
+
+                    String response = "Time:  " + parameters.get(0)
+                            + "sec . To answer? " + (parameters.get(1)==1) +". Priority number:"+ parameters.get(2);
                     System.out.println(response);
+
+                    System.out.println(queue.toString());
+
+                    if (queueFromHandler != null){
+                        System.out.println(queueFromHandler.toString());
+                    }
                     writer.write(response);
                     writer.newLine();
                     writer.flush();
@@ -33,4 +57,7 @@ public class Server {
 
 
     }
+
+
 }
+
